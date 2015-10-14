1ff27d071a8ab8fe12904675cd5f38a3cf8366c3
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-10-14 19:21:44 -0400

    initial commmit

---
+++ game.js
@@ -0,0 +1,48 @@
+// var player = prompt ("You are Bear, a brown 15lb wire-haired daschund with floppy ears, white paws and white tipped tail.  What do you want to do first? (Bark, Sniff, Play, or Nap?)").toUpperCase();
+
+// switch(player) {
+// var actions {
+//   sniff []
+//   }
+//   bark
+//   play
+//   nap
+
+  // function sniff (){
+  //   var hole = prompt ("You come upon the trail of a Badger!  You follow the smell to their den - do you go in? YES or NO?").toUpperCase();
+  //   if (hole === "YES") {
+  //     var teaParty = prompt("You see a family of badgers of having a tea party do you join in? YES or NO?").toUpperCase();
+  //       if (teaParty === "YES") {
+  //         alert("You join them for a cup of tea and then tear them to shreds");
+  //       }
+  //       else if (teaParty === "NO") {
+  //         alert("You bark at the hole till its night time and Max comes and finds you!");
+  //       }
+  //     }
+  //     if (hole === "NO") {
+  //       alert("Ok - you decide to sniff some more and find some poop.  You eat it.");
+  //     }
+  //   }
+      // break;
+    function bark(){
+      document.getElementById("choice").innerHTML = "You hear something and start  barking - mommy tells you to stop, do you? YES or NO?";
+      document.getElementById("yesNo").innerHTML;
+    }
+        if (yapper === "NO") {
+          var yapper2 = prompt ("Bark again and even louder? YES or NO?").toUpperCase();
+          if (yapper2 === "YES") {
+            alert("A piercing sound surrounds you.  Max is holding a small gray box and its pointed at me.  WHAT IS HAPPENING.. TO ... ME..?!!  You stop barking");
+          }
+          else if (yapper2 === "NO") {
+              alert("You grumble a bit, mommy doesn't understand that you can hear a school on fire and the screaming children and just want to tell her.  You remember that humans are deaf.");
+          }
+        }
+        if (yapper === 'YES') {
+        alert("Who cares for barking anyway. You decide to PLAY instead");
+        player = "PLAY";
+      }
+    }
+      // break;
+//       case 'PLAY':
+//       var butterfly = prompt ("You prance through the flowery fields and chase a butterfly, you see your tail, do you chase it? YES or NO?").toUpperCase();
+// }
---
+++ game3.js
@@ -0,0 +1,3 @@
+//SURVIVOR
+
+var player = prompt ("Being a millionaire is great - unless while in your")
