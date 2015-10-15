f37066fdf6fb2b78b57c321e1cdb80beac2b239c
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-10-14 23:34:08 -0400

    so much coding - tried a bunch of flashy stuff and ended up doing it the hard way

--- game.js
+++ game.js
@@ -6,7 +6,78 @@
 //   }
 //   bark
 //   play
-//   nap
+// //   nap
+// function postToPage(text) {
+//    var push = (document.getElementById('choice').innerHTM = "(text)"
+//  }
+// var postToPage(push){
+//   var text ="";
+//   (push) = document.getElementById('choice').innerHTML = + text;
+// }
+function bark(options){
+  if (options === 1) {
+      document.getElementById('choice').innerHTML = "You hear something and start  barking - mommy tells you to stop, do you? YES or NO?";
+      document.getElementById('yesNoBark').style.display="block";
+      document.getElementById('actionChoice').style.display="none";
+    }
+  if (options === 2) {
+      document.getElementById('choice').innerHTML = "A piercing sound surrounds you.  Max is holding a small gray box and its pointed at me.  WHAT IS HAPPENING.. TO ... ME..?!!  You stop barking";
+  }
+  if (options === 3) {
+    document.getElementById('choice').innerHTML = "You grumble a bit, mommy doesn't understand that you can hear a school on fire and the screaming children and just want to tell her.  You remember though that humans are deaf and get some scritches and kisses";
+  }
+}
+function sniff(options) {
+  if (options === 1) {
+    document.getElementById('choice').innerHTML = "You come upon the trail of a Badger!  You follow the smell to their den - do you go in?";
+    document.getElementById('yesNoSniff').style.display="block";
+    document.getElementById('actionChoice').style.display="none";
+  }
+  if (options === 2) {
+    document.getElementById('choice').innerHTML ="You see a family of badgers of having a tea party do you join in? YES or NO?";
+  }
+  if (options === 3) {
+    document.getElementById('choice').innerHTML = "Ok - you decide to sniff some more and find some poop.  You eat it.";
+    }
+  if (options === 4) {
+    document.getElementById('choice').innerHTML ="You join them for a cup of tea, have a merry time, and then and then tear them to shreds.";
+  }
+}
+function play(options) {
+  if (options === 1) {
+    document.getElementById('choice').innerHTML = "You prance through the flowery fields and chase a butterfly, you see your tail, do you chase it? YES or NO?"
+    document.getElementById('yesNoPlay').style.display="block";
+    document.getElementById('actionChoice').style.display="none";
+  }
+  if options === 2) {
+    document.getElementById('choice').innerHTML =
+  }
+  if options === 3) {
+    document.getElementById('choice').innerHTML =
+  }
+  if options === 4) {
+    document.getElementById('choice').innerHTML =
+  }
+}
+function nap(options) {
+  if (true) {
+    document.getElementById('choice').innerHTML =""
+    document.getElementById('yesNoNap').style.display="block";
+    document.getElementById('actionChoice').style.display="none";
+  }
+  if options === 4) {
+    document.getElementById('choice').innerHTML =
+  }
+  if options === 4) {
+    document.getElementById('choice').innerHTML =
+  }
+  if options === 4) {
+    document.getElementById('choice').innerHTML =
+  }
+  if options === 4) {
+    document.getElementById('choice').innerHTML =
+  }
+}
 
   // function sniff (){
   //   var hole = prompt ("You come upon the trail of a Badger!  You follow the smell to their den - do you go in? YES or NO?").toUpperCase();
@@ -24,24 +95,22 @@
   //     }
   //   }
       // break;
-    function bark(){
-      document.getElementById("choice").innerHTML = "You hear something and start  barking - mommy tells you to stop, do you? YES or NO?";
-      document.getElementById("yesNo").innerHTML;
-    }
-        if (yapper === "NO") {
-          var yapper2 = prompt ("Bark again and even louder? YES or NO?").toUpperCase();
-          if (yapper2 === "YES") {
-            alert("A piercing sound surrounds you.  Max is holding a small gray box and its pointed at me.  WHAT IS HAPPENING.. TO ... ME..?!!  You stop barking");
-          }
-          else if (yapper2 === "NO") {
-              alert("You grumble a bit, mommy doesn't understand that you can hear a school on fire and the screaming children and just want to tell her.  You remember that humans are deaf.");
-          }
-        }
-        if (yapper === 'YES') {
-        alert("Who cares for barking anyway. You decide to PLAY instead");
-        player = "PLAY";
-      }
-    }
+    // function bark(){
+    //   document.getElementById("choice").innerHTML = "You hear something and start  barking - mommy tells you to stop, do you? YES or NO?";
+    //     if (yapper === "NO") {
+    //       var yapper2 = prompt ("Bark again and even louder? YES or NO?").toUpperCase();
+    //       if (yapper2 === "YES") {
+    //         alert("A piercing sound surrounds you.  Max is holding a small gray box and its pointed at me.  WHAT IS HAPPENING.. TO ... ME..?!!  You stop barking");
+    //       }
+    //       else if (yapper2 === "NO") {
+    //           alert("You grumble a bit, mommy doesn't understand that you can hear a school on fire and the screaming children and just want to tell her.  You remember that humans are deaf.");
+    //       }
+    //     }
+    //     if (yapper === 'YES') {
+    //     alert("Who cares for barking anyway. You decide to PLAY instead");
+    //     player = "PLAY";
+    //   }
+    // }
       // break;
 //       case 'PLAY':
 //       var butterfly = prompt ("You prance through the flowery fields and chase a butterfly, you see your tail, do you chase it? YES or NO?").toUpperCase();
